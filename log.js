397c58f5777540e8e7ab6ccdfdb9d9fb55a91bb7
cassiezer0 <cciglesiasmartinez@gmail.com>
2023-07-23 13:46:21 +0200

    Very basic logging functionality.

--- log.js
+++ log.js
@@ -4,48 +4,37 @@
  */
 
 const pino = require('pino');
-const rotate = require('@chatsift/pino-rotate-file');
+const fs = require('fs');
 
 class Logger {
-  constructor(logFilePath, rotateOptions = {}) {
-    if (Logger.instance) {
-      return Logger.instance; // Singleton: Returning instance if created
+    constructor() {
+        if (Logger.instance) {
+            return Logger.instance; // Singleton: Returning instance if created
+        }
+        // Basic parameters
+        this.logFilePath = 'log/app.log';
+        this.logFileOptions = {
+            flags: 'a',
+        }
+        // Creating stream
+        this.logFileStream = fs.createWriteStream(this.logFilePath, this.logFileOptions);
+        // Instancing the logger with previous configuration
+        this.logger = pino({
+            level: 'info', // Set logging level
+            timestamp: pino.stdTimeFunctions.isoTime, // TStamp format
+        }, this.logFileStream);
+        Logger.instance = this; // Saving instance here
     }
-    // File to write 
-    this.logFilePath = 'log/app.log';
-    // Options for rotation
-    this.rotateOptions = {
-        size: '10M', 
-        keep: 5,
-        compress: 'gzip'
-    };
-    // Configure and instance rotation
-    this.logFileStream = rotate(this.logFilePath, this.rotateOptions);
-    // Pino options 
-    const prettyPrint = {
-      colorize: true,
-      translateTime: true,
-    };
-
-    // Instancing the logger with previous configuration
-    this.logger = pino({
-      level: 'info', // Set logging level
-      prettyPrint,
-      timestamp: pino.stdTimeFunctions.isoTime, // TStamp format
-    }, this.logFileStream);
-
-    Logger.instance = this; // Saving instance here
-  }
-
-  // Method for info logs
-  info(message, data = {}) {
-    this.logger.info(data, message);
-  }
-
-  // Methods for error logs
-  error(message, data = {}) {
-    this.logger.error(data, message);
-  }
+    // Method for info logs
+    info(message, data = {}) {
+        console.log(message);
+        this.logger.info(data, message);
+    }
+    // Methods for error logs
+    error(message, data = {}) {
+        console.log(message);
+        this.logger.error(data, message);
+    }
 }
 
-module.exports = Logger;
+module.exports = { Logger };
