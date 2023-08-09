6e3250cdd62c4bba88125b1eb0acf7cb56cd64d8
cassiezer0 <cciglesiasmartinez@gmail.com>
2023-08-09 14:50:22 +0200

    Some comments just in case we use this approach.

--- db2.js
+++ db2.js
@@ -33,3 +33,37 @@ class AsyncMySQLDB {
 }
 
 module.exports = { AsyncMySQLDB }
+
+// Code to invoke and use this new way of doing things
+// New database using mysql2/promises
+/*
+const database = require('./db2.js');
+
+async function initDB() {
+    const dbConfig = {
+        host: '127.0.0.1',
+        user: 'root',
+        password: 'password',
+        database: 'deafstar',
+        port: 3306,
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0
+    };
+
+    const db = new database.AsyncMySQLDB(dbConfig);
+    await db.createPool();
+
+    try {
+        const users = await db.getUsers();
+        users.forEach(user => {
+            console.log(user);
+    }); 
+    } catch (error) {
+        console.error('Error:', error.message);
+    } finally {
+        await db.closePool();
+    }
+}
+initDB();
+*/
