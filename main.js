36eb477be8c23d60e1e65e6ba8b9761e9756289e
cassiezer0 <cciglesiasmartinez@gmail.com>
2023-08-15 08:29:54 +0200

    Did some cleaning.

--- main.js
+++ main.js
@@ -9,23 +9,36 @@ const fs = require('fs');
 const http = require('http');
 const ws = require('ws');
 const app = express();
-const crawler = require('./crawler.js');
-const database = require('./db.js');
-const chat = require('./chat.js');
-const { Logger } = require('./log.js');
-const { configuration } = require('./conf.js');
+const crawler = require('./src/crawler.js');
+const database = require('./src/db.js');
+const chat = require('./src/chat.js');
+const { Logger } = require('./src/log.js');
+const { conf } = require('./conf/conf.js');
+const { UserManagement, User } = require('./src/struct.js');
 
 // Instancing database
-const db = new database.DB('127.0.0.1', 'root', 'password', 'deafstar');
+const db = new database.DB(conf.db.host, conf.db.user, conf.db.password, conf.db.name );
 
 // Initializing logger
 const logger = new Logger();
+
+// Init main class and test users (this is dirty, just temp)
+const userManagement = new UserManagement();
+
+
+
+/*
+ * Express server initialization
+ *
+ */
 
 // Retrieve SSL key + cert
 const sslFiles = {
     cert: fs.readFileSync(__dirname + '/ssl/snakeoil.crt'),
     key: fs.readFileSync(__dirname + '/ssl/snakeoil.key'),
 }
+
+// Create the instance
 const secureHttpd = http.createServer(sslFiles, app);
 
 // Setting template engine
@@ -35,65 +48,11 @@ app.set('views', __dirname + '/views');
 // This will allow the template engine to ue stuff (img/css) in the "/public" folder
 app.use(express.static(__dirname + "/public"));
 
-// Main class (this name *has to* be changed)
-class Main {
-    constructor() {
-        if (Main.instance) {
-            return Main.instance; // Singleton!
-        }
-        this.users = [];
-        Main.instance = this;
-    }
-    // @param is object instantiated from class Users
-    addUser(user) {
-        this.users.push(user);
-    }
-    // Search for an object given handler attr
-    getUser(handler) {
-        for (let i=0; i<this.users.length; i++) {
-            if ( this.users[i].handler == handler ) {
-                return this.users[i];
-            }
-        }
-    }
-    // Temp method, might be changed later
-    getUserByHandler(handler) {
-        for (let i=0; i<this.users.length; i++) {
-            if ( this.users[i].handler == handler ) {
-                return this.users[i];
-            }
-        }
-    }
-    getUserByChatId(id) {
-        console.log("CAlling the getUserByChat func");
-        for (let i=0; i<this.users.length; i++) {
-            console.log("Iterating...")
-            if (this.users[i].chatbot !== undefined ) {
-                if ( this.users[i].chatbot.chatId == id ) {
-                    console.log("Got a result!!");
-                    return this.users[i];
-                }
-            }
-        }
-        console.log("Finished func");
-    }
-    listUsers(callback) {
-        callback(this.users);
-    }
-    getPassword(handler) {
-        let password;
-        for (let i=0; i<this.users.length; i++) {
-            if (this.users[i].handler == handler) {
-                password = this.users[i].token;
-                break;
-            }
-        }
-        return password;
-    }
-}
+// Initialize and put listening the http server
+secureHttpd.listen(conf.server.port, () => {
+    logger.info('[HTTPd] Running on port ' + conf.server.port);
+});
 
-// Init main class and test users (this is dirty, just temp)
-const mainClass = new Main();
 
 
 /*
@@ -121,27 +80,29 @@ passport.serializeUser((user, done) => {
   
 // Deserialize user
 passport.deserializeUser((handler, done) => {
-    const user = mainClass.getUser(handler);
+    const user = userManagement.getUserByHandler(handler);
     done(null, user);
 });
   
-// Configuración de Passport y estrategia local
+// Passport configuration, local strategy.
 passport.use(new LocalStrategy(
     (username, password, done) => {
-        if (password == mainClass.getPassword(username)) {
+        // Regular users
+        if (password == userManagement.getPassword(username)) {
             logger.info('[HTTPS] Auth succeeded for user ' + username);
-            return done(null, mainClass.getUser(username));
+            return done(null, userManagement.getUserByHandler(username));
         }
-        if ( (username == configuration.admin.username) 
-        && (password == configuration.admin.password)) { 
+        // Administrator
+        if ( (username == conf.admin.username) 
+        && (password == conf.admin.password)) { 
             logger.info('[HTTPS] Auth succeeded for the administrator');
             const adminUserObj = new database.User(
-                configuration.admin.password,
-                configuration.admin.username,
+                conf.admin.password,
+                conf.admin.username,
                 'Administrator',
                 'admin@email.com');
-            mainClass.addUser(adminUserObj);
-            return done(null,mainClass.getUser(username));
+            userManagement.addUser(adminUserObj);
+            return done(null,userManagement.getUserByHandler(username));
         } 
         else {
             return done(null, false, { message: 'Invalid login' });
@@ -149,10 +110,46 @@ passport.use(new LocalStrategy(
     }
 ));
 
+// Middleware for authentication
+function isAlreadyAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        res.redirect('/profile');
+    } else {
+        return next();
+    }
+}
+
+// More middleware for authentication
+function isAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next(); // Is authenticated
+    }
+    res.status(404).render('404'); // Is not auth
+}
+
+/*
+// More middleware. Maybe this should be deprecated.
+function isAdmin(req, res, next) {
+    if ((req.isAuthenticated()) && (req.user == 'admin')) {
+        return next();
+    }
+    res.status(404).render('404');
+}
+*/
+
 // Initializing passport and establishing sessions
 app.use(passport.initialize());
 app.use(passport.session());
 
+
+
+/*
+ * Routes
+ * 
+ * In the future, those routes here should be re-factored and adapted
+ * to operate in a RESTful API manner.
+ *
+ */
 
 // Login route
 app.post('/login', passport.authenticate('local', {
@@ -166,7 +163,7 @@ app.post('/crawl', isAuthenticated, async (req, res) => {
     try {
         console.log(req.body.url);
         const chatBot = new chat.ChatBot(req.body.url);
-        const user = mainClass.getUser(req.user.handler);
+        const user = userManagement.getUserByHandler(req.user.handler);
         await chatBot.initialize(req.body.url,user);
         console.log(req.body.url, user);
         console.log(chatBot);
@@ -174,20 +171,16 @@ app.post('/crawl', isAuthenticated, async (req, res) => {
 
     } catch (err) {
         console.error(err);
-        res.redirect('/profile?');
+        res.redirect('/profile');
     }
 });
 
-// These two routes (temp and system) should be merged <3
- 
 // System route
 app.post('/system', isAuthenticated, async (req, res) => {
     //Logic for changing the system msg
     try {
-        console.log(req.body.url);
-        const user = mainClass.getUser(req.user.handler);
+        const user = userManagement.getUserByHandler(req.user.handler);
         user.systemMsg = req.body.url;
-        console.log(user)
         const query = `
             UPDATE users SET system_msg = ? WHERE id = ?
         `;
@@ -196,7 +189,7 @@ app.post('/system', isAuthenticated, async (req, res) => {
         res.redirect('/profile');
     } catch (err) {
         console.error(err);
-        res.redirect('/profile?');
+        res.redirect('/profile');
     }
 });
 
@@ -204,10 +197,8 @@ app.post('/system', isAuthenticated, async (req, res) => {
 app.post('/temp', isAuthenticated, async (req, res) => {
     //Logic for changing the temperature
     try {
-        console.log(req.body.url);
-        const user = mainClass.getUser(req.user.handler);
+        const user = userManagement.getUserByHandler(req.user.handler);
         user.temp = Number(req.body.url);
-        console.log(user)
         const query = `
             UPDATE users SET temp = ? WHERE id = ?
         `;
@@ -216,7 +207,7 @@ app.post('/temp', isAuthenticated, async (req, res) => {
         res.redirect('/profile');
     } catch (err) {
         console.error(err);
-        res.redirect('/profile?');
+        res.redirect('/profile');
     }
 });
 
@@ -225,30 +216,6 @@ app.get('/logout', (req, res) => {
     req.logout(() => {console.log("Logged out");});
     res.redirect('/');
 });
-
-
-// Change this func names, they're a bit confusing
-function isAlreadyAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        res.redirect('/profile');
-    } else {
-        return next();
-    }
-}
-
-function isAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next(); // Is authenticated
-    }
-    res.status(404).render('404'); // Is not auth
-}
-
-function isAdmin(req, res, next) {
-    if ((req.isAuthenticated()) && (req.user == 'admin')) {
-        return next();
-    }
-    res.status(404).render('404');
-}
 
 // Login page
 app.get('/login', isAlreadyAuthenticated, (req, res) => {
@@ -265,7 +232,7 @@ app.get('/', (req,res) => {
 app.get('/profile', (req, res) => {
     if (req.isAuthenticated()) {
         const username = req.user.handler;
-        if ( username == configuration.admin.username) {
+        if ( username == conf.admin.username) {
             res.redirect('/admin');
         } else { 
             res.redirect(`/${username}`);
@@ -278,8 +245,7 @@ app.get('/profile', (req, res) => {
 // Getting admin panel
 app.get('/admin', isAuthenticated, (req, res) => { 
     const data = 'testing';
-    console.log(req.user);
-    if ( req.user.handler == configuration.admin.username ) {
+    if ( req.user.handler == conf.admin.username ) {
         res.render('admin', { data })
     }
     else {
@@ -291,8 +257,8 @@ app.get('/admin', isAuthenticated, (req, res) => {
 app.get('/chat/:id', (req,res) => {
     const { id } = req.params;
     console.log(id);
-    const user = mainClass.getUserByChatId(id);
-    if (mainClass.getUserByChatId(id) !== undefined) {
+    const user = userManagement.getUserByChatId(id);
+    if (userManagement.getUserByChatId(id) !== undefined) {
         console.log("Got an user");
         console.log(user);
         res.render('chat', { user });
@@ -304,13 +270,13 @@ app.get('/:username', isAuthenticated, (req, res) => {
     const { username } = req.params;
     const { handler } = req.user;
     // Checking if exists
-    if ((mainClass.getUser(username)) && (handler == username)) {
-        const user = mainClass.getUser(username);
+    if ((userManagement.getUserByHandler(username)) && (handler == username)) {
+        const user = userManagement.getUserByHandler(username);
         console.log("[HTTPd]: Obtained user \n", user);
         res.render('user', { user });
     } else {
         // Sending error
-        //console.log(mainClass.users, "Requested user " + req.params.username);
+        //console.log(userManagement.users, "Requested user " + req.params.username);
         res.status(404).render('404');
     }
 });
@@ -320,11 +286,6 @@ app.use((req, res, next) => {
     res.status(404).render('404');
 });
 
-// Init the http server
-secureHttpd.listen(8008, () => {
-    const port = 8008;
-    logger.info('[HTTPd] Running on port ' + port);
-});
 
 
 /* 
@@ -333,46 +294,41 @@ secureHttpd.listen(8008, () => {
  */
 
 // Init websocket server through https
-
 const wsServer = new ws.Server({server: secureHttpd});
 
 // WS functions
-
 wsServer.on('connection',  (ws) => {
     logger.info("[WEBSOCKET] Client connected"); 
     //const greet = {origin: "server", data: { response: { content: "Hello, how can I help you?" } } };
     //ws.send(JSON.stringify(greet));
     ws.on('message', async (msg) => {
+        console.log("[WEBSOCKET] Data from " + msg.origin + " received: " + msg.data);
         try {
+            // Parse the msg and get the object for the user
             msg = JSON.parse(msg);
-            console.log("[WEBSOCKET] Data from " + msg.origin + " received: " + msg.data);
-            console.log("VERSION WITHOUT CIENT")
-            // Calling OpenAI API
-            let user = mainClass.getUserByHandler(msg.origin);
+            const user = userManagement.getUserByHandler(msg.origin);
+            // Check if user has not a chatbot attached (still not have crawled their site)
             if ( user.chatbot === undefined) {
                 const response = await chat.generateText(msg.data);
-                console.log("THIS SHOULD BE SHOWING!!!");
-                const myRes = {
+                const dataObj = {
                     response: { 
                         role: undefined,
-                        content: response },
+                        content: response 
+                    },
                     urls: ["Unavailable", "Unavailable"]
                 }
-                console.log(response);
                 const resMsg = {
                     origin: "server",
-                    data: myRes,
+                    data: dataObj,
                 }
                 console.log(resMsg);
+                // We send the response in JSON format to the WebSocket client.
                 ws.send(JSON.stringify(resMsg));
             } else {
-                console.log("ENTERING HERE WHILE I SHOULDNT");
-                console.log("[WEBSOCKET] Data from " + msg.origin + " received: " + msg.data);
+
                 let chatbot = user.chatbot;
-                //console.log(chatbot);
-                console.log("Got the bot");
-                console.log(user);
                 let resMsg;
+                // We check if current user has debug mode enabled or not.
                 if ( msg.debug == true ) {
                     let text = await chatbot.generateText(msg.data, user, msg.debug);
                     resMsg = {
@@ -387,12 +343,10 @@ wsServer.on('connection',  (ws) => {
                         data: text,
                     }
                 }
-
                 console.log(resMsg);
+                // We send the response in JSON format to the WebSocket client.
                 ws.send(JSON.stringify(resMsg));
             }
-
-            
         } catch (err) {
             console.error("[WEBSOCKET] Error in JSON msg: " + err)
         }
@@ -400,18 +354,21 @@ wsServer.on('connection',  (ws) => {
 });
 
 
-// For first time running
 
+/* 
+ * Database initialization.
+ * 
+ */
+
+// For first time running
 db.createStructure();
 
-
 // Retrieving users
-
 db.getUsers((users) => {
     users.forEach(async (user) => {
         // Add the user
         try {
-            mainClass.addUser(user);
+            userManagement.addUser(user);
             // Check if user has a chatbot and add if yes
             if (user.url !== null) {
                 const chatbot = new chat.ChatBot();
